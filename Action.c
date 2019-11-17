4162c8cd66ce3bb5c5e9c48e283a317950854589
unknown <Avishek@DESKTOP-QC2B9UV>
2019-11-17 11:50:17 +0530

    1st commit

---
+++ Action.c
@@ -0,0 +1,49 @@
+	web_custom_request("web_custom_request",
+		"URL=www.example.com",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+	Action()
+{
+
+	web_set_sockets_option("SSL_VERSION", "TLS1.2");
+
+	web_add_auto_header("DNT", 
+		"1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_url("blazedemo.com", 
+		"URL=http://blazedemo.com/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		EXTRARES, 
+		"Url=/favicon.ico", "Referer=", ENDITEM, 
+		LAST);
+
+	web_add_auto_header("Origin", 
+		"http://blazedemo.com");
+
+	web_submit_form("reserve.php", 
+		"Snapshot=t2.inf", 
+		ITEMDATA, 
+		"Name=fromPort", "Value=Paris", ENDITEM, 
+		"Name=toPort", "Value=London", ENDITEM, 
+		LAST);
+
+	web_submit_form("purchase.php", 
+		"Ordinal=2", 
+		"Snapshot=t3.inf", 
+		ITEMDATA, 
+		LAST);
+
+	return 0;
+}
